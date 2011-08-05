674d7119ab4be25d8012157e3b4ebb22e47eed27
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2011-08-05 15:07:43 +0900

    ama-oto.org
    ama-oto.org / ~2011.04.26

---
+++ js/common.js
@@ -0,0 +1,3 @@
+document.write('<script type="text/javascript" src="js/jquery.js"></script>');
+document.write('<script type="text/javascript" src="js/scroll.js"></script>');
+document.write('<script type="text/javascript" src="js/facebox.js"></script>');
