e347ff37397af6aecb691bccdb030705f2b866af
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2012-04-16 18:35:37 +0900

    とりあえず初期化

--- js/common.js
+++
@@ -1,3 +0,0 @@
-document.write('<script type="text/javascript" src="js/jquery.js"></script>');
-document.write('<script type="text/javascript" src="js/scroll.js"></script>');
-document.write('<script type="text/javascript" src="js/facebox.js"></script>');
